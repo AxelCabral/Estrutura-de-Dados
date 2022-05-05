ec1befec9540602710f36b40a1c33b30bcc219c3
Axel Cabral <axel.cabral0102@gmail.com>
2022-05-05 15:28:43 -0300

    Arranjos-Alunos v1.0

---
+++ arranjos1ED/ConjuntoAlunos.java
@@ -0,0 +1,24 @@
+package arranjos1ED;
+
+public class ConjuntoAlunos {
+    private Aluno[] alunos = new Aluno[10];
+    private int totalAlunos = 0;
+
+    public void adiciona(Aluno aluno){
+        this.alunos[this.totalAlunos] = aluno;
+        this.totalAlunos++;
+    }
+
+    public int tamanho(){
+        return this.totalAlunos;
+    }
+
+    public boolean contem(Aluno aluno){
+        for(int i=0; i<this.totalAlunos; i++){
+            if(aluno == this.alunos[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ arranjos1ED/main.java
@@ -0,0 +1,17 @@
+package arranjos1ED;
+
+public class main{
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Axel", 20, 10.0f);
+        Aluno a2 = new Aluno("Lucas", 19, 5.9f);
+        ConjuntoAlunos alunos = new ConjuntoAlunos();
+        alunos.adiciona(a1);
+        System.out.println((alunos.tamanho()));
+        System.out.println((alunos.contem(a1)));
+        System.out.println((alunos.contem(a2)));
+        //System.out.println(a1.getNome());
+        //System.out.println(a1.getIdade());
+        //System.out.println(a1.getNota());
+    }
+}
