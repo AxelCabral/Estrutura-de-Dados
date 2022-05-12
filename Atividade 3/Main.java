aa0e47a288f19e827390d5d292f9212213b56d4a
Axel Cabral <axel.cabral0102@gmail.com>
2022-05-12 17:29:07 -0300

    atividade3p
    atividade3 - parcial

---
+++ Atividade 3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("Axel", 20, 10.0f);
+        Aluno a2 = new Aluno("Lucas", 19, 5.9f);
+        ConjuntoAlunos alunos = new ConjuntoAlunos();
+        alunos.adiciona(a1);
+        alunos.adiciona(a2);
+        System.out.println((alunos.tamanho()));
+        System.out.println((alunos.contem(a1)));
+        System.out.println((alunos.contem(a2)));
+        
+    }
+}
